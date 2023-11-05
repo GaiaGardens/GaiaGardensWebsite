635dab62230d5244bc41e5d36804690e48e6565e
GaiaGardens <121769924+GaiaGardens@users.noreply.github.com>
2023-11-05 14:44:46 -0800

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,10 +1,22 @@
-
+/*Mobile Menu*/
 $(document).ready(function(){
     var w = document.documentElement.clientWidth || window.innerWidth;
     if (w <= 500) {
         $("#main-nav-header").css(({"display":"none"}));
         $("#hamburger").click(function(){
             $("#main-nav-header").toggle();
+        });
+    } else {
+        ScrollReveal().reveal('nav', {delay:1000, duration: 1000});
+    }
+});
+
+$(document).ready(function(){
+    var w = document.documentElement.clientWidth || window.innerWidth;
+    if (w <= 500) {
+        $("#alternate-nav-header").css(({"display":"none"}));
+        $("#hamburger").click(function(){
+            $("#alternate-nav-header").toggle();
         });
     } else {
         ScrollReveal().reveal('nav', {delay:1000, duration: 1000});
