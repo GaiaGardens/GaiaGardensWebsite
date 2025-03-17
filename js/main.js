91e1cafc6dd2cfbe34e7a0a1f036f3d8e656dca0
GaiaGardens <121769924+GaiaGardens@users.noreply.github.com>
2025-03-16 20:26:28 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -178,58 +178,58 @@ $(document).ready(function(){
     $("#image-item-1").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4meNRd58I_vov14uC-RSm7V1c0SVhO7bYQYM2c1qs4NZlbzahZNpRwcDIeJ7d5HeHMGok3p2ZtkgbxQN7DHwbNyNq0goO5GaPYPa_9XWJ8Iy-2KsBTpyuyHp6Tul_Qt0ZW-7CRS8HcWeQN7C8QsU6sHg5skp0OlLC7ycvO-5kAvquITGYLkppgqmkBApDQH_gD?width=3024&height=4032&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166719/gallery9_iggwnv.jpg)" , });
     });
 
     $("#image-item-2").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4m-BuexTNboPWJq9ycVIHqx0R59cj454gRzneH42qNJeOadi_QcOo4l0UYtBQthHnVHJvHkLA9DcI1vN3EQhfnONtsm2lb2SOhgtMRSAmpv4Mr9dbE6FiPfZ1vQxaJ_wjz24Mbvehso6FbuJggHzdhPXL33BhTtZ3bwJeM_Q4ZA93eSgInKhYMa2TX6_dVBQFJ?width=3024&height=3318&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166712/gallery2_nngdcm.jpg)" , });
     });
 
     $("#image-item-3").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mogC_GhqGDJzq5Ws1T4RbF77j9M4Q6yM0NZia4z4UvKXJSL3EwpkryKausBy52hIlyix7Y9jwsF9V0GD6MSpv12X27_lkRACRBcPnqiqG0ZE4f6t6BtWh2AJ7bmje7sw3wvzNi4qTtp7lc_MjB8dfM6hYpdlCIIi7KFOS7W0YRfUKeeRabpjzTpmYp-QEEeMH?width=4032&height=3024&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166708/gallery1_ebu0of.jpg)" , });
     });
 
     $("#image-item-4").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4m6G8Jc9-65LnHqb2pVTO5pVe3CBjNBZpOjOjtoTsZxq3GNZv3Rr8qoEecEGHTJQF0uWHXDwGJhvzlomNqJxiIIBen-dFcv1aIchACSMklv6HlEwwEEJzTgVhUHqdTMo7Fo4wshznWQEJw6cX7y2YUCWGRuo8_ZEelwPmlrcwlGDes3-6KHL2tDOcOmO90tO22?width=3469&height=2647&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166716/gallery6_hdzcdb.jpg)" , });
     });
 
     $("#image-item-5").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mcRA8MkX-_PKRPseO6BBX5BubyrKF37JKPtYvqitjABV0mlL3DYLkYvbEKd9BD3sBfqvPhntRXLXWsYGWq7UO0LMo9Oud08QUo6ioCTdAslJ59YzxbOgqRis-Rat2L-6qrm4-ltsQWEOWo5d-8gkzh0ycjSqnRqbK-1z0-coGx3xJDDBT3BFm9V8jd_gXuIxj?width=3024&height=4032&cropmode=none)" , });
-    });
-
-    $("#image-item-5").click(function(){
-        $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
-        $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mcRA8MkX-_PKRPseO6BBX5BubyrKF37JKPtYvqitjABV0mlL3DYLkYvbEKd9BD3sBfqvPhntRXLXWsYGWq7UO0LMo9Oud08QUo6ioCTdAslJ59YzxbOgqRis-Rat2L-6qrm4-ltsQWEOWo5d-8gkzh0ycjSqnRqbK-1z0-coGx3xJDDBT3BFm9V8jd_gXuIxj?width=3024&height=4032&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166715/gallery4_ytvb7i.jpg)" , });
     });
 
     $("#image-item-6").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mJiCaN-BDN1QAX_SEi8iQr_jvn78fUQo4LdREsdrCAYrRcYk28VxUiaw1vsL2ew43VGzWrm9aBFYieehWOfSRbQWX3AtLWbrQQ5At2OfyoO9Re8BjM12F2m85oaPVG0jH9XcgA17XDWTLGms49_TpGr-Gev0V73AtH4nrg8z1pBBAnrItGk38tsL3o1koSMEr?width=3024&height=4032&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166714/gallery3_t604ft.jpg)" , });
     });
 
     $("#image-item-7").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mnKWDxYXXxoriaQO86olz6a3w5K8YytVcNXJPPkrDXCG4gWYqCDRja0Mn7BaAQ50y0h8AFTwm7SPZ-q68TBD_qmPv9PvztVVOOR5F5JUmRO8Qu0yV5r98g1ZcaqFGZbhOEeXWyF5lVFAXQjidQmbpLMXiYFCK0qXcCDUowhQuM4-_ws-n36-_gK6nGdxzmn7P?width=3024&height=4032&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166717/gallery7_rqysd7.jpg)", });
     });
 
     $("#image-item-8").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
-        $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4molgt21s7AoxsY0GZzqpZ7dCQkdLT_j7Pi8ROswWfPyEzlLCgtDQXXZbgY-8in-TYlR8wwbHQJLtYlJhD6DcHvMBTXJF4esSeZiCSSgF25nwrOgpnyLxZUQCwNBt5zabaSWvTitK1YdtnW9yoHfyM30YCpE4Tz9C4AzGUBTcv6XbK9sqhRytIYWr7bvkaEXhI?width=3469&height=2647&cropmode=none)" , });
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166711/gallery12_pqhkkn.jpg)" , });
     });
 
     $("#image-item-9").click(function(){
+        $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
+        $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
+        $(".overlay-carousel-picture").css({"background-image": "url(https://res.cloudinary.com/dr3r301ht/image/upload/v1742166715/gallery5_y251ps.jpg)" , });
+    });
+
+    $("#image-item-10").click(function(){
         $(".dark-overlay").css({"visibility":"visible", "opacity":".7"});
         $(".overlay-carousel-container").css({"visibility":"visible", "opacity":"1"});
         $(".overlay-carousel-picture").css({"background-image": "url(https://dsm01pap009files.storage.live.com/y4mvLTYv6elUtkWBW1b6ELAlzkrrqkmX_YLZJVhKtO-y_HLufL9arpZvDaruBR-0kQ386HMW0rSO2wbI16u8LL6r45RCxfWOOl_mZfJ6J3nV6ZJUXhWM0civgMaI60z_kycRRbhUrNL3nnO1_2fWDU7OuKwO0RsVo3BdvwkAoKYHo3uYqZrPHk5idJ0Ffxn2khJ?width=3024&height=4032&cropmode=none)" , });
